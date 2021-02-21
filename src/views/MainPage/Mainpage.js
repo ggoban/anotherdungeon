89a4849c8ed9d320d821f422511d105bdbe9a043
Lee Hyunuk <as0266@naver.com>
2021-02-21 21:30:25 +0900

    Fix UI

--- src/views/MainPage/Mainpage.js
+++ src/views/MainPage/Mainpage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row } from 'antd';
+import { Row, Col, Button } from 'antd';
 import {withRouter} from "react-router-dom";
 import Footer from "../Footer/Footer";
 import MenuBar from "../utils/MenuBar";
@@ -20,6 +20,16 @@ function Mainpage() {
         <div style={{paddingTop: "4rem"}} className="container">
             <Row align="middle">
                 {menus.map((menu, index) => ( <MenuBar key={index} menu={menu}/>))}
+                <Col xs={24} md={12} style={{margin: "1rem auto"}}>
+                    <a href="https://hu-lee.github.io/anothercharcheck/" target="_blank" rel="noreferrer">
+                    <Button style={{ height: "60px", width: "80%", fontSize: "1.5rem", lineHeight:"50px", fontWeight: 600}}>
+                            <div style={{display: "flex", justifyContent: "center"}}>
+                                <img src="images/character_base/메리나(ES).png" alt="메리나" style={{height: "50px", marginRight: "10px"}} />
+                                캐릭터 체크리스트
+                            </div>
+                        </Button>
+                    </a>
+                </Col>
             </Row>
             <br/>
             <h2><b>이경은 (정말로) 추가 예정이 없습니다.</b></h2>
--- src/views/utils/MenuBar.js
+++ src/views/utils/MenuBar.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import { Card, Col } from 'antd';
+import { Col, Button } from 'antd';
 import { Link } from "react-router-dom";
 
 function MenuBar({menu}) {
     return (
-        <Col xs={24} md={12}>
+        <Col xs={24} md={12} style={{margin: "1rem auto"}}>
             <Link to={`/${menu.url}`}>
-                <Card hoverable
-                    style={{width: "80%", margin: "0 auto", display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}
-                    headStyle= {{width: '10%'}}
-                    bodyStyle= {{width: '90%'}}
-                    cover={<img alt={menu.pic} src={`images/character_base/${menu.pic}.png`}/>}
-                >
-                    <b style={{fontSize: "1.1rem"}}>{menu.title}</b>
-                </Card>
+                <Button style={{ height: "60px", width: "80%", fontSize: "1.5rem", lineHeight:"50px", fontWeight: 600}}>
+                    <div style={{display: "flex", justifyContent: "center"}}>
+                        <img src={`images/character_base/${menu.pic}.png`} alt={menu.pic} style={{height: "50px", marginRight: "10px"}} />
+                        {menu.title}
+                    </div>
+                </Button>
             </Link>
         </Col>
     )
